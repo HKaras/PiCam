fad0b081aafcc63ccaf87ea5b99e1776b44778c7
Hugo Karas <hugokaras@gmail.com>
2017-07-16 11:45:36 +0100

    Update Shoot.py

--- Shoot.py
+++ Shoot.py
@@ -4,7 +4,7 @@ import time
 
 #camera setup
 camera = picamera.PiCamera()
-timestr = time.strftime("%Y%m%d-%H%M%S)
+timestr = time.strftime("%Y%m%d-%H%M%S")
 camera.resolution = (2592, 1944)
 camera.start_preview()
 #Camera warm up time
